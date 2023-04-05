10b88b4558d3d9e23fe647e06bff565b07e2874d
hanl <sloner@yeah.net>
2023-04-06 03:34:16 +0800

    更改主题样式

--- themes/next/components/Footer.js
+++ themes/next/components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import BLOG from '@/blog.config'
 
-const Footer = ({ title }) => {
+const Footer = () => {
   const d = new Date()
   const currentYear = d.getFullYear()
   const copyrightDate = (function() {
@@ -19,12 +19,6 @@ const Footer = ({ title }) => {
         <i className='fas fa-copyright' /> {`${copyrightDate}`} <span className='mx-1 animate-pulse'><i className='fas fa-heart' /></span> <a href={BLOG.LINK} className='underline font-bold '>{BLOG.AUTHOR}</a>.<br />
 
         {BLOG.BEI_AN && <><i className='fas fa-shield-alt' /> <a href='https://beian.miit.gov.cn/' className='mr-2'>{BLOG.BEI_AN}</a><br /></>}
-
-        <span className='hidden busuanzi_container_site_pv'>
-          <i className='fas fa-eye' /><span className='px-1 busuanzi_value_site_pv'> </span> </span>
-        <span className='pl-2 hidden busuanzi_container_site_uv'>
-          <i className='fas fa-users' /> <span className='px-1 busuanzi_value_site_uv'> </span> </span>
-        <h1>{title}</h1>
       </span>
     </footer>
   )
