c52898592881864fb9487a2782b41e231a27bd97
hanl <sloner@yeah.net>
2023-04-06 19:13:54 +0800

    更改个人卡片

--- themes/next/components/InfoCard.js
+++ themes/next/components/InfoCard.js
@@ -1,13 +1,24 @@
 import BLOG from '@/blog.config'
-import Router from 'next/router'
 import React from 'react'
 import SocialButton from './SocialButton'
+import { useGlobal } from '@/lib/global'
+import { saveDarkModeToCookies } from '@/lib/theme'
 
 const InfoCard = (props) => {
   const { siteInfo } = props
+  const { isDarkMode, updateDarkMode } = useGlobal()
+  // 用户手动设置主题
+  const handleChangeDarkMode = () => {
+    const newStatus = !isDarkMode
+    saveDarkModeToCookies(newStatus)
+    updateDarkMode(newStatus)
+    const htmlElement = document.getElementsByTagName('html')[0]
+    htmlElement.classList?.remove(newStatus ? 'light' : 'dark')
+    htmlElement.classList?.add(newStatus ? 'dark' : 'light')
+  }
   return <>
     <div className='flex flex-col items-center justify-center '>
-        <div className='hover:rotate-45 hover:scale-125 transform duration-200 cursor-pointer' onClick={ () => { Router.push('/') }}>
+        <div className='hover:rotate+45 hover:scale-125 transform duration-200 cursor-pointer' onClick={ handleChangeDarkMode }>
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img src={siteInfo?.icon} className='rounded-full' width={120} alt={BLOG.AUTHOR}/>
         </div>
