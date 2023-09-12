a5e91a3ad3589625e5f7ab9c48ef49d0e3ec5eae
ilzxm <ilzxm@outlook.com>
2023-09-12 20:08:15 +0800

    移除无用引用

--- components/DarkModeButton.js
+++ components/DarkModeButton.js
@@ -1,6 +1,5 @@
 import { useGlobal } from '@/lib/global'
 import { saveDarkModeToCookies } from '@/themes/theme'
-import { Moon, Sun } from './HeroIcons'
 import { useImperativeHandle } from 'react'
 
 /**
