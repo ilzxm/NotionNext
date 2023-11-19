31b7e0a3e57685ac6e72267d9611060081c81b2f
ilzxm <ilzxm@outlook.com>
2023-11-19 23:09:36 +0800

    头像点击事件

--- themes/next/components/InfoCard.js
+++ themes/next/components/InfoCard.js
@@ -1,13 +1,18 @@
 import LazyImage from '@/components/LazyImage'
-import Router from 'next/router'
 import SocialButton from './SocialButton'
 import { siteConfig } from '@/lib/config'
+import { useGlobal } from '@/lib/global'
 
 const InfoCard = (props) => {
   const { siteInfo } = props
+  const { toggleDarkMode } = useGlobal()
+  // 用户手动设置主题
+  const handleChangeDarkMode = () => {
+    toggleDarkMode()
+  }
   return <>
     <div className='flex flex-col items-center justify-center '>
-        <div className='hover:rotate-45 hover:scale-125 transform duration-200 cursor-pointer' onClick={ () => { Router.push('/') }}>
+        <div className='hover:rotate-180 hover:scale-125 transform duration-200 cursor-pointer' onClick={ handleChangeDarkMode }>
         <LazyImage src={siteInfo?.icon} className='rounded-full' width={120} alt={siteConfig('AUTHOR')}/>
         </div>
         <div className='text-2xl font-serif dark:text-white py-2 hover:scale-105 transform duration-200'>{siteConfig('AUTHOR')}</div>
